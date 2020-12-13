b9fe58c33528f114156c3d50f82050b2762e1f9a
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-13 20:21:07 +0300

    added equals and hashcode for department class

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -2,6 +2,8 @@ package models;
 
 import com.sun.org.apache.xpath.internal.objects.XString;
 
+import java.util.Objects;
+
 public class Department {
 
     private int id;
@@ -15,6 +17,52 @@ public class Department {
         this.empnumber = empnumber;
     }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname;
+    }
+
+    public int getEmpnumber() {
+        return empnumber;
+    }
+
+    public void setEmpnumber(int empnumber) {
+        this.empnumber = empnumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id &&
+                empnumber == that.empnumber &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(deptname, that.deptname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, deptname, empnumber);
+    }
 }
 
