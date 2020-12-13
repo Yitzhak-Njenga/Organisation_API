1bfee30d5b498154f2c70b32390d6b50d600e30a
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-13 23:10:30 +0300

    added constructors for news class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -5,4 +5,10 @@ public class News {
     private String wriitenBy;
     private String cotent;
     private int deptid;
+
+    public News(String wriitenBy, String cotent, int deptid) {
+        this.wriitenBy = wriitenBy;
+        this.cotent = cotent;
+        this.deptid = deptid;
+    }
 }
