763368313f8c3216c417e0b016d4be606a321003
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-13 06:50:17 +0300

    added department class

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,11 @@
+package models;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Department {
+
+    private int id;
+    private String description;
+    private String deptname;
+    private int empnumber;
+}
