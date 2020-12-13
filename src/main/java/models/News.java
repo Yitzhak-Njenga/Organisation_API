8da52477907524e9d8db373eb6535e2f0607f9a3
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-13 23:12:00 +0300

    added getter and setters for news class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -11,4 +11,36 @@ public class News {
         this.cotent = cotent;
         this.deptid = deptid;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWriitenBy() {
+        return wriitenBy;
+    }
+
+    public void setWriitenBy(String wriitenBy) {
+        this.wriitenBy = wriitenBy;
+    }
+
+    public String getCotent() {
+        return cotent;
+    }
+
+    public void setCotent(String cotent) {
+        this.cotent = cotent;
+    }
+
+    public int getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(int deptid) {
+        this.deptid = deptid;
+    }
 }
