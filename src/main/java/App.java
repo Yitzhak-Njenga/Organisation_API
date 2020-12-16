20b2314d6c413ec0616e053f50795238c06a7f6b
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-12-16 11:57:18 +0300

    added a coonection to the db

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import com.google.gson.Gson;
+import dao.Sql2oOrganisationDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class App {
+
+    public static void main (String[] args){
+        Sql2oOrganisationDao newDao;
+        Connection connection;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:posgresql:locolhost:5432/departments";
+        Sql2o sql2o = new Sql2o(connectionString,"moringa","1234");
+    }
 }
